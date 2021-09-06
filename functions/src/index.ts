f671bc32981dfd6682006e177249fd7724418dcc
marusho <mrsho34@gmail.com>
2021-09-07 07:28:27 +0900

    typescript の導入

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,15 @@
-//"use strict";
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
 admin.initializeApp(functions.config().firebase);
-
-// データベースの参照を作成
-var fireStore = admin.firestore()
 
 // localテスト時に設定
 // fireStore.settings({
