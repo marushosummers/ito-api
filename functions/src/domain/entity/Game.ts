ae3182d3c48946943069b2d337dad7be586afa0f
marusho <mrsho34@gmail.com>
2021-09-07 22:31:20 +0900

    Playerドメインを作成

--- functions/src/domain/entity/Game.ts
+++ functions/src/domain/entity/Game.ts
@@ -1,18 +1,29 @@
 export class Game {
   private id: string
   private thema: string
-  private players: string[]
+  private players: Player[]
   private status: GameStatus
-  private history: string[]
 
-  public constructor(props: {id: string, thema: string, players: string[], status: GameStatus, history: string[]}) {
-    const {id, thema, players, status, history} = props;
+  public constructor(props: { id: string, thema: string, players: Player[], status: GameStatus}) {
+    const {id, thema, players, status} = props;
     this.id = id;
     this.thema = thema;
     this.players = players;
     this.status = status;
-    this.history = history;
   }
 }
 
 export type GameStatus = "INPLAY" | "OVER" | "SUCCESS" | "QUIT";
+
+class Player {
+  public readonly id: string
+  public readonly card: number
+  public readonly isPlayed: Boolean
+
+  public constructor(props: {id: string, card: number, isPlayed: Boolean}) {
+    const { id, card, isPlayed } = props;
+    this.id = id;
+    this.card = card;
+    this.isPlayed = isPlayed;
+  }
+}
